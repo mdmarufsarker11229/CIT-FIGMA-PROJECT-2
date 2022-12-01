544567eb9bd3693334adf07d7386769bb51b39ca
mdmarufsarker11229 <mjsarkarmj454@gmail.com>
2022-12-01 20:11:08 +0600

    CIT-FIGMA-PROJECT-2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.slider_item').slick({
+        slidesToShow:5,
+        slidesToScroll:2,
+        arrows:false,
+        centerMode:true,
+        centerPadding:"100px",
+        focusOnSelect:true,
+        autoplay:false,
+        autoplaySpeed:100,
+
+    })
+})
